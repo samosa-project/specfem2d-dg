d8282671e1cb74284cca4ee3418634098c13d453
Léo Martire <leo.martire@outlook.com>
2018-06-25 13:50:16 +0200

    Added safeguard when using DG: one cannot set a coord_interface (in parfile) below (<, <= is ok) mesh minimum altitude anymore.

--- src/specfem2D/setup_mesh.F90
+++ src/specfem2D/setup_mesh.F90
@@ -932,6 +932,33 @@ subroutine setup_mesh_surface_DG_coupling()
     write(IMAIN,*) 'Zmin,Zmax of the whole mesh = ',zmin,zmax
     write(IMAIN,*)
   endif
+  
+  ! Safeguard when using discontinuous method: output a warning if coord_interface>zmin, and stop program with an error if coord_interface<zmin.
+  if(USE_DISCONTINUOUS_METHOD) then
+    if(coord_interface>zmin) then
+      write(*,*) "********************************"
+      write(*,*) "*           WARNING            *"
+      write(*,*) "********************************"
+      write(*,*) "* coord_interface>zmin, which  *"
+      write(*,*) "* is curious. Be wary of       *"
+      write(*,*) "* spurious behaviours,         *"
+      write(*,*) "* especially when using bottom *"
+      write(*,*) "* forcing.                     *"
+      write(*,*) "* coord_interface = ", coord_interface
+      write(*,*) "* zmin            = ", zmin
+      write(*,*) "********************************"
+    elseif(coord_interface<zmin) then
+      write(*,*) "********************************"
+      write(*,*) "*            ERROR             *"
+      write(*,*) "********************************"
+      write(*,*) "* coord_interface<zmin, which  *"
+      write(*,*) "* is nonsense.                 *"
+      write(*,*) "* coord_interface = ", coord_interface
+      write(*,*) "* zmin            = ", zmin
+      write(*,*) "********************************"
+      stop
+    endif ! Endif on coord_interface and zmin.
+  endif
 
   ! checks that no source is located outside the mesh
   if (myrank == 0) then
