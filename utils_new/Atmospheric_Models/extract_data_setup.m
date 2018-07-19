3e25bfda1708d6bff868f36375bf53280fab95c2
Léo Martire <leo.martire@outlook.com>
2018-07-19 10:49:21 +0200

    Matlab scripts update.

--- utils_new/Atmospheric_Models/extract_data_setup.m
+++ utils_new/Atmospheric_Models/extract_data_setup.m
@@ -18,12 +18,47 @@ function [datestr, posstr, year, daysincenewyear, secondssincenewday, lat, lon, 
     if(count==1)
       year=str2num(regexprep(regexprep(line, 'day.*', ''), 'year', ''));
       daysincenewyear=str2num(regexprep(regexprep(line, 'seconds.*', ''), 'year *[0-9]+ day', ''));
-      secondssincenewday=str2num(regexprep(regexprep(line, 'lat.*', ''), 'year *[0-9]+ day *[0-9]+ seconds', ''));
-      datestr=[num2str(daysincenewyear), 'th day of ', num2str(year), ' at ' num2str(floor(secondssincenewday/3600)),':',num2str(floor((secondssincenewday - floor(secondssincenewday/3600)*3600)/60)), ' UT'];
-
       lat=str2num(regexprep(regexprep(line, 'year *[0-9]+ day *[0-9]+ seconds *[0-9]+\.[0-9]+ *lat', ''), 'lon.*', ''));
       lon=str2num(regexprep(line, 'year *[0-9]+ day *[0-9]+ seconds *[0-9]+\.[0-9]+ *lat *-?[0-9]+\.[0-9]+ *lon', ''));
-      posstr=['lat. ',num2str(lat), ', lon. ',num2str(lon)];
+      if(lon<=180)
+        lonstr=[sprintf('%1.1f',lon),'$^\circ$E'];
+      else
+        lonstr=[sprintf('%1.1f',360-lon),'$^\circ$W'];
+      end
+      if(lat>=0)
+        latstr=[sprintf('%1.1f',lat),'$^\circ$N'];
+      else
+        latstr=[sprintf('%1.1f',-lat),'$^\circ$S'];
+      end
+      
+      % Build position string.
+%       posstr=['(lat., lon.) = (',latstr,', ',lonstr,')'];
+      posstr=['(',latstr,', ',lonstr,')'];
+      
+      % Deduce month and day.
+      prevsum=0;
+      sum=0;
+      m=1;
+      while(sum<daysincenewyear)
+        prevsum=sum;
+        sum=sum+eomday(year,m);
+        m=m+1;
+      end
+      m=m-1;
+      dom=daysincenewyear-prevsum;
+      yyyymmdd=[num2str(year),'/',pad(num2str(m),2,'left','0'),'/',pad(num2str(dom),2,'left','0')];
+      % Build hh:mm:ss.
+      secondssincenewday=str2num(regexprep(regexprep(line, 'lat.*', ''), 'year *[0-9]+ day *[0-9]+ seconds', ''));
+      hhmm=[pad(num2str(floor(secondssincenewday/3600)),2,'left','0'),':',pad(num2str(floor((secondssincenewday - floor(secondssincenewday/3600)*3600)/60)),2,'left','0')];
+      hhmmss=[hhmm,':','00'];
+      % Convert to local time using longitude.
+      CoordinatedUniversalTimeStr=[yyyymmdd,' ',hhmmss];
+      localsat=UTC2SolarApparentTime(CoordinatedUniversalTimeStr,lon); % See https://fr.mathworks.com/matlabcentral/fileexchange/32804-convert-utc-to-solar-apparent-time.
+      splitlocalsat=split(localsat, ' ');
+      % Build date string.
+%       datestr=[num2str(daysincenewyear), '$^{th}$ of ', num2str(year), ', ',num2str(floor(secondssincenewday/3600)),':',num2str(floor((secondssincenewday - floor(secondssincenewday/3600)*3600)/60)), ' UT'];
+      datestr=[yyyymmdd, ', ', hhmmss, ' UT, ', splitlocalsat{2}, ' LT'];
+      
     elseif(count==2)
       f107a=str2num(regexprep(regexprep(line, '.*F107A', ''), 'F107.*', ''));
       f107=str2num(regexprep(regexprep(line, '.*F107', ''), 'AP.*', ''));
--- utils_new/Atmospheric_Models/plot_model.m
+++ utils_new/Atmospheric_Models/plot_model.m
@@ -5,27 +5,58 @@
 % Usage:         N/A.
 % Notes:         N/A.
 
-function [] = plot_model(output_file, subplotcode, marker, colour, legends)
-  [Z, RHO, ~, C, ~, ~, ...
-   ~, NSQ, ~, ~, ~, WN, WE, ~, ~, ~, GAMMA] = ...
+function [] = plot_model(output_file, marker, colour, atmalts)
+  format compact;
+  set(0, 'DefaultLineLineWidth', 3); set(0, 'DefaultLineMarkerSize', 8);
+  set(0, 'defaultTextFontSize', 26); set(0, 'defaultAxesFontSize', 28);
+  set(0, 'DefaultTextInterpreter', 'latex');
+  set(0, 'DefaultLegendInterpreter', 'latex');
+  
+  [Z, RHO, T, C, ~, ~, ...
+   ~, NSQ, ~, ~, ~, ~, ~, W, ~, ~, GAMMA] = ...
    extract_data(output_file, 3, 0, 0);
 
-  [~, posstr, ~, d, s, ~, ~, F107A, F107, AP] = extract_data_setup(output_file);
-  apf107str=strcat("F10.7 avg. ", sprintf("%.1f",F107A), ", F10.7 ", sprintf("%.1f",F107), ", AP ", sprintf("%.1f",AP));
-%       datestr=[num2str(y),', ',num2str(d), 'th day, ', num2str(floor(s/3600)),':',num2str(floor((s - floor(s/3600)*3600)/60)), ' UT'];
-  datestr=[num2str(d), 'th day, ', num2str(floor(s/3600)),':',num2str(floor((s - floor(s/3600)*3600)/60)), ' UT'];
-
+  [datestr, posstr, ~, d, s, ~, ~, F107A, F107, AP] = extract_data_setup(output_file);
+  apf107str=strcat("F10.7 avg. = ", sprintf("%.1f",F107A), ", F10.7 = ", sprintf("%.1f",F107), ", AP = ", sprintf("%.1f",AP));
+  
+  T=T-273.15;
+  
+%   D = differentiation_matrix(Z, 0);
+%   DZW = D*W;
+%   DZW(1)=DZW(2); % Correction hack.
+  DZW=gradient(W,Z);
+  DZW(1:2)=DZW(3); % Correction hack.
+  
   figure();
-  subplot(subplotcode+1); myplot(RHO, Z, marker, colour, datestr, 'sx'); xlabel('$\rho$ (kg/m$^3$)'); % xlim([0.1*min(RHO),10*max(RHO)]);
-  subplot(subplotcode+2); myplot(C, Z, marker, colour, datestr, 'p'); xlabel('$c$ (m/s)'); % LIMX=C; xlim([1.1*min(LIMX)-0.1*max(LIMX),1.1*max(LIMX)-0.1*min(LIMX)]);
-  title({[posstr],apf107str,''});
-  subplot(subplotcode+3); myplot(NSQ, Z, marker, colour, datestr, 'p'); xlabel('$N^2$ (rad$^2$/s$^2$)'); % LIMX=NSQ; xlim([1.1*min(LIMX)-0.1*max(LIMX),1.1*max(LIMX)-0.1*min(LIMX)]);
-  if(strcmp(legends,'no')==0)
-    legend('Location', 'northwest');
+  
+  ax(1)=subplot(231); myplot(RHO, Z, marker, colour, datestr, 'sx'); xlabel('$\rho$ (kg/m$^3$)'); ylabel('$z$ (m)'); % xlim([0.1*min(RHO),10*max(RHO)]);
+  
+  ax(2)=subplot(232);
+  myplot(C, Z, marker, colour, '$c$', 'p'); xlabel('$c$ (m/s)'); hold on
+  myplot(min(C+W,C-W), Z, marker, 'b', 'upwind $c_e$', 'p');
+  myplot(max(C+W,C-W), Z, marker, 'r', 'downwind $c_e$', 'p');
+  legend('Location', 'best');
+  
+  title({[posstr,', ',datestr],apf107str,''});
+  
+  ax(3)=subplot(233);
+  myplot(NSQ, Z, marker, colour, datestr, 'p'); xlabel('$N^2$ (rad$^2$/s$^2$)'); % LIMX=NSQ; xlim([1.1*min(LIMX)-0.1*max(LIMX),1.1*max(LIMX)-0.1*min(LIMX)]);
+  
+  ax(4)=subplot(234);
+  myplot(T, Z, marker, colour, datestr, 'p'); xlabel('$T$ ($^\circ$C)'); ylabel('$z$ (m)'); % LIMX=WN; xlim([1.1*min(LIMX)-0.1*max(LIMX),1.1*max(LIMX)-0.1*min(LIMX)]);
+  for i=1:length(atmalts)
+    line([min(T), max(T)],[atmalts(i),atmalts(i)],'linestyle',':','color','k','linewidth',2);
   end
-  subplot(subplotcode+4); myplot(WN, Z, marker, colour, datestr, 'p'); xlabel('$w_M$ (m/s)'); % LIMX=WN; xlim([1.1*min(LIMX)-0.1*max(LIMX),1.1*max(LIMX)-0.1*min(LIMX)]);
-  subplot(subplotcode+5); myplot(WE, Z, marker, colour, datestr, 'p'); xlabel('$w_Z$ (m/s)'); % LIMX=WE; xlim([1.1*min(LIMX)-0.1*max(LIMX),1.1*max(LIMX)-0.1*min(LIMX)]);
-  subplot(subplotcode+6); myplot(GAMMA, Z, marker, colour, datestr, 'p'); xlabel('$\gamma$'); % LIMX=GAMMA; xlim([1.1*min(LIMX)-0.1*max(LIMX),1.1*max(LIMX)-0.1*min(LIMX)]); legend('Location', 'best');
+  
+  ax(5)=subplot(235);
+  myplot(W, Z, marker, colour, datestr, 'p'); xlabel('$w$ (m/s)');
+  line([0,0],[Z(1),Z(end)],'linestyle',':','color','k','linewidth',2);
+  
+  ax(6)=subplot(236);
+  myplot(DZW, Z, marker, colour, datestr, 'p'); xlabel('$\partial_zw$ (1/s)');
+  line([0,0],[Z(1),Z(end)],'linestyle',':','color','k','linewidth',2);
+  
+  linkaxes(ax,'y');
 end
 
 function myplot(XDATA, YDATA, MARKER, COLOUR, DISPLAYNAME, TYPE)
@@ -35,4 +66,5 @@ function myplot(XDATA, YDATA, MARKER, COLOUR, DISPLAYNAME, TYPE)
     semilogx(XDATA, YDATA, MARKER, 'Color', COLOUR, 'DisplayName', DISPLAYNAME); hold on;
   end
   ylim([min(YDATA), max(YDATA)]);
+  set(gca, 'TickLabelInterpreter','latex');
 end
