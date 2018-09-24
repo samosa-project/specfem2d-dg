9a6971725486bc8ebd3bda61652648134c05bfe3
Léo Martire <leo.martire@outlook.com>
2018-09-24 16:07:59 +0200

    Update EXTMSH (default example files).

--- EXAMPLES/default_example_DG/EXTMSH/autoConvertToSPECFEM.sh
+++ EXAMPLES/default_example_DG/EXTMSH/autoConvertToSPECFEM.sh
@@ -1,10 +1,42 @@
 #!/bin/bash
-
-scriptName="LibGmsh2Specfem_convert_Gmsh_to_Specfem2D_official.py"
+# Author:        Léo Martire.
+# Mail:          leo.martire@outlook.com
+# Description:   GMSH meshing pipeline for SPECFEM2D.
+# Last modified: See file metadata.
+# Usage:         1) If called with 'auto' as argument, will automatically mesh and convert to SPECFEM standards.
+#                2) If called with another or no argument, will open GMSH GUI enabling the user to mesh as they want, then convert to SPECFEM standards on closing of the GUI.
+# Notes:         N/A.
 
 # Save the current directory.
 thisScriptDir=$(dirname $(realpath $0))
 
+###############################################
+# Converting .geo to .msh                     #
+###############################################
+if [[ "$1" == auto ]]; then
+  echo "[$0] Automatic meshing."
+  gmsh $thisScriptDir/*.geo -2
+else
+  echo "[$0] Manual meshing."
+  gmsh -open $thisScriptDir/*.geo
+fi
+  
+
+###############################################
+# Eventually open GMSH to show mesh.          #
+###############################################
+if [[ "$1" == auto ]]; then
+  meshToShow="extMesh"
+  echo "[$0] GMSH opened to show $meshToShow."
+  gmsh -merge $thisScriptDir/$meshToShow.geo $thisScriptDir/$meshToShow.msh&
+fi
+
+###############################################
+# Converting mesh to SPECFEM standards.       #
+###############################################
+echo "[$0] Converting to SPECFEM standards."
+# Convert script.
+scriptName="LibGmsh2Specfem_convert_Gmsh_to_Specfem2D_official.py"
 # Go through the folders containing this folder to find the conversion script.
 found=-1
 cd $thisScriptDir
@@ -14,10 +46,8 @@ do
   fullPathToScript=$(tree -fiP "$scriptName" | grep "$scriptName")
   found=$?
 done
-
 # Save the path to the conversion script.
 absolutePathToScript="$(pwd)/$fullPathToScript"
-
 # Ask user for boundaries' types input.
 echo "> Boundaries?"
 echo "  > Top?"
@@ -28,15 +58,12 @@ echo "  > Bottom?"
 read bb
 echo "  > Right?"
 read br
-
 # Convert user's inputs to uppercase.
 bt=$(echo $bt | tr a-z A-Z)
 bl=$(echo $bl | tr a-z A-Z)
 bb=$(echo $bb | tr a-z A-Z)
 br=$(echo $br | tr a-z A-Z)
-
 # Go back to the initial directory.
 cd $thisScriptDir
-
 # Run the conversion script.
 python $absolutePathToScript *.msh -t $bt -l $bl -b $bb -r $br
--- EXAMPLES/default_example_DG/EXTMSH/extMesh.geo
+++ EXAMPLES/default_example_DG/EXTMSH/extMesh.geo
@@ -14,18 +14,18 @@ Mesh.RecombineAll = 1; // Recombine all triangular elements.
 
 // Meshing algorithm. //-----//
 Mesh.Algorithm = 9; // 1: MeshAdapt. 5: Delaunay for quads. 9: structured (experimental).
-Mesh.ElementOrder = 1; // Element order.
+Mesh.ElementOrder = 2; // Element order.
 
 // Geometry. //--------------//
-xmin=-10;
+xmin=-50;
 xmax=-xmin;
-zmin=0;
-zmax=20;
+zmin=-50;
+zmax=-zmin;
 
 // Define elements' length.
 Mesh.CharacteristicLengthFactor = 1; // Scaling of all elements' sizes.
-dxbot=1;
-dxtop=3;
+dxbot=10;
+dxtop=20;
 
 // Define geometry keypoints.
 Point(1) = {xmin,zmin,0};
@@ -42,14 +42,17 @@ Line Loop(5) = {1,2,3,4};
 // Define a surface with the previously defined line loop.
 Plane Surface(6) = {5};
 
-Physical Line("Top") = {4};
-Physical Line("Left") = {3};
-Physical Line("Bottom") = {1};
-Physical Line("Right") = {2};
+Physical Line("Top") = {4}; // Name boundary (for later conversion to SPECFEM standards).
+Physical Line("Left") = {3}; // Name boundary (for later conversion to SPECFEM standards).
+Physical Line("Bottom") = {1}; // Name boundary (for later conversion to SPECFEM standards).
+Physical Line("Right") = {2}; // Name boundary (for later conversion to SPECFEM standards).
+Physical Surface("M1") = {6}; // Name surface (for later conversion to SPECFEM standards).
+Recombine Surface {6};
 
-Physical Surface("M1") = {6};
-
-Characteristic Length {1, 2} = 2*dxbot; // Set mesh characteristic size at some points. *2 to prevent it from being too small due to first subdivision.
+Characteristic Length {1, 2} = 2*dxbot;
 Characteristic Length {4, 3} = 2*dxtop;
 
-Recombine Surface {6};
+// If a strictly structured (constant dX) mesh is wanted, uncomment and configure the two following lines.
+// Note: this overrides the previously defined characteristic lengths and Mesh.Algorithm.
+//Transfinite Line{1,2,3,4} = 5; // Let n be the number on the RHS. 2*(n-1) nodes will be created on the curve. See "http://gmsh.info/doc/texinfo/gmsh.html#index-Transfinite-Curve-_007b-expression_002dlist_002dor_002dall-_007d-_003d-expression-_003c-Using-Progression-_007c-Bump-expression-_003e_003b".
+//Transfinite Surface{6};
