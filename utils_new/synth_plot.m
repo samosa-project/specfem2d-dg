6c9c342451cdc1d43c3c2c0c644962a6455ca81f
Léo Martire <leo.martire@outlook.com>
2018-10-19 16:38:25 +0200

    Ran code beautifier.

--- utils_new/synth_plot.m
+++ utils_new/synth_plot.m
@@ -16,185 +16,185 @@ set(0, 'defaultTextFontSize', 12); set(0, 'defaultAxesFontSize', 12);
 set(0, 'DefaultTextInterpreter', 'latex');
 set(0, 'DefaultLegendInterpreter', 'latex');
 
-if(not(exist('synth_load_was_ran') && synth_load_was_ran==1))
-  error("[ERROR] synth_load was not ran before.");
+if (not(exist('synth_load_was_ran') && synth_load_was_ran == 1))
+  error(['[', mfilename, ', ERROR] synth_load was not ran before.']);
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Load.                       %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-data_t=Ztime;
-data_v=Zamp;
+data_t = Ztime;
+data_v = Zamp;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Ask for user input.         %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-rescale=-1;
-while(rescale==-1)
-  rescale=input('  Rescale (0 for no, new value for yes)? > ');
+rescale = - 1;
+while (rescale == - 1)
+  rescale = input(['[', mfilename, '] Rescale (0 for no, new value for yes)? > ']);
 end
-if(rescale~=0)
-  data_v=data_v*rescale;
-  disp(['  [WARNING] Data was rescaled by a factor ',num2str(rescale),'.']);
+if (rescale ~= 0)
+  data_v = data_v * rescale;
+  disp(['[', mfilename, ', WARNING] Data was rescaled by a factor ', num2str(rescale), '.']);
 end
 
-filter_data=-1;
+filter_data = - 1;
 % while(not(ismember(filter_data,[0,1,2,3])))
-%   filter_data=input('  Filter (0 for no, 1 for high-pass, 2 for low-pass, 3 for band-pass)? > ');
-while(not(ismember(filter_data,[0,1])))
-  filter_data=input('  Filter (0 for no, 1 for high-pass)? > ');
+%   filter_data=input(['  Filter (0 for no, 1 for high-pass, 2 for low-pass, 3 for band-pass)? > ']);
+while (not(ismember(filter_data, [0, 1])))
+  filter_data = input(['[', mfilename, '] Filter (0 for no, 1 for high-pass)? > ']);
 end
-if(filter_data~=0)
-  filter_fcutoff=-1;
-  while(filter_fcutoff<=0)
-    filter_fcutoff=input('  Filter cutoff frequency? > ');
+if (filter_data ~= 0)
+  filter_fcutoff = - 1;
+  while (filter_fcutoff <= 0)
+    filter_fcutoff = input(['[', mfilename, '] Filter cutoff frequency? > ']);
   end
   switch filter_data
     case 1
-      for(i=1:nstat)
-        [~, data_v_HP] = custom_filter(data_t(i,:), data_v(i,:), filter_fcutoff);
-        data_v(i,:)=data_v_HP;
-      end
-      disp(['  [WARNING] Data was high-pass filtered, with cutoff frequency ',num2str(filter_fcutoff),'.']);
-      clear('data_v_HP');
-    otherwise
-      disp(['  [ERROR] Filtering type not implemented.']);
+      for(i = 1:nstat)
+      [~, data_v_HP] = custom_filter(data_t(i, :), data_v(i, :), filter_fcutoff);
+      data_v(i, :) = data_v_HP;
   end
+  disp(['[', mfilename, ', WARNING] Data was high-pass filtered, with cutoff frequency ', num2str(filter_fcutoff), '.']);
+  clear('data_v_HP');
+ otherwise
+  error(['[', mfilename, ', ERROR] Filtering type not implemented.']);
 end
-fign=-1;
-fign=input('  Figure number? > ');
-colour=-1;
-colour=input('  Colour choice? ([0, 0.447, 0.741] for nice blue, [0.851, 0.325, 0.098] for nice orange, or classical notations) > ','s');
-if(length(colour)>1)
-  eval(['colour=',colour]);
 end
-dist_unit="-1";
-while(~ismember(dist_unit,["m", "km"]))
-  dist_unit=input('  Distance unit (m, km)? > ', 's');
+fign = - 1;
+fign = input(['[', mfilename, '] Figure number? > ']);
+colour = - 1;
+colour = input(['[', mfilename, '] Colour choice? ([0, 0.447, 0.741] for nice blue, [0.851, 0.325, 0.098] for nice orange, or classical notations) > '], 's');
+if (length(colour) > 1)
+  eval(['colour=', colour]);
 end
-dist_factor=1;
-distancechoice=-1;
-while(~ismember(distancechoice,[1,2,3,4]))
-  distancechoice=input('  Distance choice? (1 for x, 2 for |x|, 3 for z, 4 for d) > ');
+dist_unit = "-1";
+while (~ ismember(dist_unit, ["m", "km"]))
+  dist_unit = input(['[', mfilename, '] Distance unit (m, km)? > '], 's');
 end
-normalise=-1;
-while(~ismember(normalise,[0,1]))
-  normalise=input('  Normalise data? (0 for no, 1 for yes) > ');
+dist_factor = 1;
+distancechoice = - 1;
+while (~ ismember(distancechoice, [1, 2, 3, 4]))
+  distancechoice = input(['[', mfilename, '] Distance choice? (1 for x, 2 for |x|, 3 for z, 4 for d) > ']);
+end
+normalise = - 1;
+while (~ ismember(normalise, [0, 1]))
+  normalise = input(['[', mfilename, '] Normalise data? (0 for no, 1 for yes) > ']);
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Treatment.                  %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-if(strcmp(dist_unit, "km"))
-  dist_factor=1000;
+if (strcmp(dist_unit, "km"))
+  dist_factor = 1000;
 end
-dist_unit=strcat(" (",dist_unit,")");
+dist_unit = strcat(" (", dist_unit, ")");
 switch distancechoice
   case 1
-    distance=xstattab; dist_symbol="x"; dist_name="horizontal distance";
+    distance = xstattab; dist_symbol = "x"; dist_name = "horizontal distance";
   case 2
-    distance=abs(xstattab); dist_symbol="|x|"; dist_name="horizontal distance";
+    distance = abs(xstattab); dist_symbol = "|x|"; dist_name = "horizontal distance";
   case 3
-    distance=ystattab; dist_symbol="z"; dist_name="altitude";
+    distance = ystattab; dist_symbol = "z"; dist_name = "altitude";
   case 4
-    distance=dist_to_sources; dist_symbol="d"; dist_name="distance";
+    distance = dist_to_sources; dist_symbol = "d"; dist_name = "distance";
 end
-distance=distance/dist_factor;
+distance = distance / dist_factor;
 % Sort according to chosen distance.
-[~,isort]=sort(distance(istattab(1:nstat)));
+[~, isort] = sort(distance(istattab(1:nstat)));
 
 % Remove mean value.
-for i=1:nstat
+for i = 1:nstat
   % Enventually normalise.
-  if(normalise==1)
-    data_v(i,:)=(data_v(i,:)-min(data_v(i,:)))/(max(data_v(i,:))-min(data_v(i,:)));
+  if (normalise == 1)
+    data_v(i, :) = (data_v(i, :) - min(data_v(i, :))) / (max(data_v(i, :)) - min(data_v(i, :)));
   end
-  data_v(i,:)=data_v(i,:)-mean(data_v(i,:));
+  data_v(i, :) = data_v(i, :) - mean(data_v(i, :));
 end
 
 % Plotting tools.
-if(nstat==1)
-  dist_over_ptp=1;
+if (nstat == 1)
+  dist_over_ptp = 1;
 else
-  dist_over_ptp=max(diff(distance(istattab(isort))))/max(peak2peak(data_v(isort,:),2));
+  dist_over_ptp = max(diff(distance(istattab(isort)))) / max(peak2peak(data_v(isort, :), 2));
 end
-if(dist_over_ptp>1e15)
-  error("Variable dist_over_ptp is > 1e15, probably coming from the signal being very small everywhere.");
+if (dist_over_ptp > 1e15)
+  error(['[', mfilename, ', ERROR] Variable dist_over_ptp is > 1e15, probably coming from the signal being very small everywhere for one of the signals.']);
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Figure.                     %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 figure(fign);
-name={};
-yticklabel=[];
-scale=-1;
-while(scale~=0)
+name = {};
+yticklabel = [];
+scale = - 1;
+while (scale ~= 0)
   close(fign);
   figure(fign);
-  if(scale~=0 && scale~=-1)
-    dist_over_ptp=scale;
+  if (scale ~= 0 && scale ~= - 1)
+    dist_over_ptp = scale;
   end
   % Prepare a string to give information about what is plotted.
-  coef_string=sprintf("%.2e",dist_over_ptp);
-  spls=split(coef_string,"e");
-  coef_string=[char(spls(1)),'\cdot10^{',num2str(str2num(char(spls(2)))),'}'];
+  coef_string = sprintf(" %.2e",dist_over_ptp);
+  spls = split(coef_string, "e");
+  coef_string = [char(spls(1)), '\cdot10^{', num2str(str2num(char(spls(2)))), '}'];
   clear('spls');
-  for i=1:nstat
-    istat=isort(i);
+  for i = 1:nstat
+    istat = isort(i);
     istat_glob = istattab(istat);
-%     vertical_shift{istat}=*distance(istat_glob);
-    name{istat}=strcat('S', num2str(istat_glob));
-    yticklabel=[yticklabel, sprintf("%.2f",distance(istat_glob))];
-    plot(data_t(istat,:),distance(istat_glob)+dist_over_ptp*data_v(istat,:),'displayname',name{istat},'color',colour);
+    %     vertical_shift{istat}=*distance(istat_glob);
+    name{istat} = strcat('S', num2str(istat_glob));
+    yticklabel = [yticklabel, sprintf(" %.2f",distance(istat_glob))];
+    plot(data_t(istat, :), distance(istat_glob) + dist_over_ptp * data_v(istat, :), 'displayname', name{istat}, 'color', colour);
     hold on;
   end
   xlim([min(data_t(1:nstat, 1)), max(data_t(1:nstat, end))]);
   xlabel('time (s)');
-  ylabel(strcat("$",dist_symbol," + \left(",coef_string,"\right)\times$", unknown_name));
-  scale=input('  Rechoose coefficient (0 for no, new value for yes)? > ');
+  ylabel(strcat("$", dist_symbol, " + \left(", coef_string, "\right)\times$", unknown_name));
+  scale = input(['[', mfilename, '] Rechoose coefficient (0 for no, new value for yes)? > ']);
 end
 
-ylabel(strcat(dist_name," $",dist_symbol,"$ ", dist_unit, ' $\longrightarrow$'));%, " $\longrightarrow$"));
-set(gca, 'TickLabelInterpreter','latex');
+ylabel(strcat(dist_name, " $", dist_symbol, "$ ", dist_unit, ' $\longrightarrow$')); %, " $\longrightarrow$"));
+set(gca, 'TickLabelInterpreter', 'latex');
 
 % Time axis limits.
-tmin=input(['  t_min (',num2str(min(min(data_t))),' now)? > ']);
-tmax=input(['  t_max (',num2str(max(max(data_t))),' now)? > ']);
-if(isempty(tmin))
-  tmin=min(min(data_t));
+tmin = input(['[', mfilename, '] t_min (', num2str(min(min(data_t))), ' now)? > ']);
+tmax = input(['[', mfilename, '] t_max (', num2str(max(max(data_t))), ' now)? > ']);
+if (isempty(tmin))
+  tmin = min(min(data_t));
 end
-if(isempty(tmax))
-  tmax=max(max(data_t));
+if (isempty(tmax))
+  tmax = max(max(data_t));
 end
 xlim([tmin, tmax]);
 
 % Eventually label each plot.
-labeleach=-1;
-while(~ismember(labeleach,[0,1]))
-  labeleach=input('  Label each line? (0 for no, 1 for yes) > ');
+labeleach = - 1;
+while (~ ismember(labeleach, [0, 1]))
+  labeleach = input(['[', mfilename, '] Label each line? (0 for no, 1 for yes) > ']);
 end
-if(labeleach==1)
+if (labeleach == 1)
   % YTicks.
   yticks(distance(istattab(isort)));
   % Stations' names labels.
-  for i=1:size(isort,1)
-    text(1.01*tmax,distance(istattab(isort(i))),name{isort(i)},'HorizontalAlignment','left');
+  for i = 1:size(isort, 1)
+    text(1.01 * tmax, distance(istattab(isort(i))), name{isort(i)}, 'HorizontalAlignment', 'left');
   end
 end
 
 % Scales.
-if(normalise==0)
-  scale=-1;
-  while(not(ismember(scale,[0,1])))
-    scale=input('  Draw scale examples? (0 for no, 1 for yes)? > ');
+if (normalise == 0)
+  scale = - 1;
+  while (not(ismember(scale, [0, 1])))
+    scale = input(['[', mfilename, '] Draw scale examples? (0 for no, 1 for yes)? > ']);
   end
-  if(scale==1)
-    min_amplitude_log10=floor(log10(max(max(data_v(:,:)')-min(data_v(:,:)'))));
-    curxlim=get(gca,'xlim');
-    for i=1:10
-      absc=curxlim(1)+diff(curxlim)*i/11;
-      plot(absc*[1,1],distance(istattab(isort(1)))*[1,1]+dist_over_ptp*0.5*i*10^(min_amplitude_log10)*[-1,1],'DisplayName',sprintf("%.0e",i*10^(min_amplitude_log10)));
+  if (scale == 1)
+    min_amplitude_log10 = floor(log10(max(max(data_v(:, :)')-min(data_v(:,:)'))));
+    curxlim = get(gca, 'xlim');
+    for i = 1:10
+      absc = curxlim(1) + diff(curxlim) * i / 11;
+      plot(absc * [1, 1], distance(istattab(isort(1))) * [1, 1] + dist_over_ptp * 0.5 * i * 10 ^ (min_amplitude_log10) * [- 1, 1], 'DisplayName', sprintf(" %.0e",i*10^(min_amplitude_log10)));
     end
   end
 end
@@ -203,7 +203,7 @@ end
 % ax=gca();
 % set(ax, 'Position', [1,1,0.9,1].*ax.Position);
 
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Clear variables.             %
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 clear('coef_string', 'colour', 'data_t', 'data_v', 'dist_factor', 'dist_name', 'dist_over_ptp', 'distancechoice', 'fign', 'i', 'isort', 'istat', 'istat_glob', 'labeleach', 'name', 'normalise', 'scale', 'spls', 'tmax', 'tmin', 'yticklabel');
--- utils_new/synth_psd.m
+++ utils_new/synth_psd.m
@@ -3,8 +3,7 @@
 % Description:   Computes PSDs and NPSDs of synthetics.
 % Last modified: See file metadata.
 % Usage:         N/A.
-% Notes:         /utils_new/synth_load.m should have been ran
-%                before.
+% Notes:         /utils_new/synth_load.m should have been ran before.
 
 % clear all;
 % close all;
@@ -21,7 +20,7 @@ addpath('/home/l.martire/Documents/SPECFEM/specfem-dg-master/utils_new');
 % Load.                       %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % TODO: ask for user input.
-raw_t=Ztime; raw_s=Zamp; cd(OFd);
+raw_t = Ztime; raw_s = Zamp; cd(OFd);
 % raw_t=stf_from_run(:, 1)'; raw_s=stf_from_run(:, 2)';
 % raw_t=data_leo_t(1,:);raw_s=data_leo_v(1,:);
 % OKQ0
@@ -40,14 +39,14 @@ raw_t=Ztime; raw_s=Zamp; cd(OFd);
 % Parameters and              %
 % pre-treatment.              %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-normalise_wpsd=-1;
-while(not(length(normalise_wpsd)==1 && ismember(normalise_wpsd,[0,1])))
-  normalise_wpsd=input('  Normalise Welch PSD (0 for no, 1 for yes)? > ');
+normalise_wpsd = - 1;
+while (not(length(normalise_wpsd) == 1 && ismember(normalise_wpsd, [0, 1])))
+  normalise_wpsd = input(['[', mfilename, '] Normalise Welch PSD (0 for no, 1 for yes)? > ']);
 end
-if(normalise_wpsd)
-  normalise_wpsd_txt=" (normalised)";
+if (normalise_wpsd)
+  normalise_wpsd_txt = " (normalised)";
 else
-  normalise_wpsd_txt="";
+  normalise_wpsd_txt = "";
 end
 
 % Eventually, compute integral or derivative.
@@ -57,82 +56,82 @@ end
 
 % TODO: Ask for user input.
 % signal_name = "SIGNAL"; signal_unit="UNIT";
-signal_name = "$\delta P$"; signal_unit="Pa";
+signal_name = "$\delta P$"; signal_unit = "Pa";
 
-avgwpsds=-1;
-if(nstat>1)
-  while(not(length(avgwpsds)==1 && ismember(avgwpsds,[0,1,2,3,4])))
-    avgwpsds=input('  Multiple data found. Choose first (0), average WPSDs (1), compute WPSD of average signal (2), plot every WPSD on top of each other (3), or plot every PSD as surf (4)? > ');
+avgwpsds = - 1;
+if (nstat > 1)
+  while (not(length(avgwpsds) == 1 && ismember(avgwpsds, [0, 1, 2, 3, 4])))
+    avgwpsds = input(['[', mfilename, '] Multiple data found. Choose first (0), average WPSDs (1), compute WPSD of average signal (2), plot every WPSD on top of each other (3), or plot every PSD as surf (4)? > ']);
   end
   switch(avgwpsds)
     case 0
-      disp('  Will compute Welch PSD of first data.');
-      WPSD_txt=strcat("Welch PSD of " ,signal_name, " [",signal_unit,"$^2$/Hz]",normalise_wpsd_txt);
-      IDs_to_process=1;
+      disp(['[', mfilename, '] Will compute Welch PSD of first data.']);
+      WPSD_txt = strcat("Welch PSD of ", signal_name, " [", signal_unit, "$^2$/Hz]", normalise_wpsd_txt);
+      IDs_to_process = 1;
     case 1
-      disp('  Averaging Welch PSDs. Be wary of the stations you use.');
-      WPSD_txt=strcat("Average of Welch PSDs of " ,signal_name, " [",signal_unit,"$^2$/Hz]",normalise_wpsd_txt);
-      IDs_to_process=1:nstat;
+      disp(['[', mfilename, '] Averaging Welch PSDs. Be wary of the stations you use.']);
+      WPSD_txt = strcat("Average of Welch PSDs of ", signal_name, " [", signal_unit, "$^2$/Hz]", normalise_wpsd_txt);
+      IDs_to_process = 1:nstat;
     case 2
-      disp('  Computing Welch PSD of average signal. Be wary of the stations you use.');
-      WPSD_txt=strcat("Welch PSD of averaged " ,signal_name, " [",signal_unit,"$^2$/Hz]",normalise_wpsd_txt);
-      IDs_to_process=1;
+      disp(['[', mfilename, '] Computing Welch PSD of average signal. Be wary of the stations you use.']);
+      WPSD_txt = strcat("Welch PSD of averaged ", signal_name, " [", signal_unit, "$^2$/Hz]", normalise_wpsd_txt);
+      IDs_to_process = 1;
     case 3
-      disp('  Plotting every PSD on top of each other.');
-      WPSD_txt=strcat("Welch PSDs of " ,signal_name, " [",signal_unit,"$^2$/Hz]",normalise_wpsd_txt);
-      IDs_to_process=1:nstat;
+      disp(['[', mfilename, '] Plotting every PSD on top of each other.']);
+      WPSD_txt = strcat("Welch PSDs of ", signal_name, " [", signal_unit, "$^2$/Hz]", normalise_wpsd_txt);
+      IDs_to_process = 1:nstat;
     case 4
-      disp('  Plotting every PSD as surf.');
-      WPSD_txt=strcat("Welch PSDs of " ,signal_name, " [",signal_unit,"$^2$/Hz]",normalise_wpsd_txt);
-      IDs_to_process=1:nstat;
+      disp(['[', mfilename, '] Plotting every PSD as surf.']);
+      WPSD_txt = strcat("Welch PSDs of ", signal_name, " [", signal_unit, "$^2$/Hz]", normalise_wpsd_txt);
+      IDs_to_process = 1:nstat;
   end
 else
-  WPSD_txt=strcat("Welch PSD of " ,signal_name, " [",signal_unit,"$^2$/Hz]",normalise_wpsd_txt);
-  IDs_to_process=1;
+  WPSD_txt = strcat("Welch PSD of ", signal_name, " [", signal_unit, "$^2$/Hz]", normalise_wpsd_txt);
+  IDs_to_process = 1;
 end
 
-if(ismember(avgwpsds,[1,2]))
-  if(avgwpsds==2)
-    raw_s=mean(raw_s,1);
+if (ismember(avgwpsds, [1, 2]))
+  if (avgwpsds == 2)
+    raw_s = mean(raw_s, 1);
   end
-  timeseries_txt=strcat("Averaged ", signal_name, " (",signal_unit,")");
+  timeseries_txt = strcat("Averaged ", signal_name, " (", signal_unit, ")");
 else
-  timeseries_txt=strcat(signal_name, " (",signal_unit,")");
+  timeseries_txt = strcat(signal_name, " (", signal_unit, ")");
 end
 
-WPSD_tab=[];
-for i=IDs_to_process
+WPSD_tab = [];
+for i = IDs_to_process
   % Set signal to be used.
-  signal = raw_s(i,:);
+  signal = raw_s(i, :);
 
   % Select time frame.
   % TODO: ask for user input.
-  select_time_l=raw_t(i,1); select_time_u=raw_t(i,end);
+  select_time_l = raw_t(i, 1); select_time_u = raw_t(i, end);
   % select_time_l=raw_t(1); select_time_u=48;
   % select_time_l=0; select_time_u=2.9;
 
   % Select.
   % time=raw_t; time = time(select_time_l<=time); time=time(time<=select_time_u); signal = signal(time<=select_time_u); % Cut signal and time around selection window.
-  time=raw_t(i,:); signal(select_time_l>=time)=0; signal(time>=select_time_u)=0; % Zero signal value around selection window.
+  time = raw_t(i, :); signal(select_time_l >= time) = 0; signal(time >= select_time_u) = 0; % Zero signal value around selection window.
   % signal=[signal, zeros(1,size(time,2))]; time=[time-time(1), time(end)-2*time(1)+time]; % Zero signal value around selection window and add zeros at the end.
 
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   % PSD and plot.               %
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-  [WPSD,WPSD_f]=custom_psd(time, signal);
+  [WPSD, WPSD_f] = custom_psd(time, signal);
 
-  WPSD_tab(i,:)=WPSD;
+  WPSD_tab(i, :) = WPSD;
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % "Stack".                    %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-if(ismember(avgwpsds,[0,1,2]))
-  timeseries_to_plot = mean(raw_s(IDs_to_process,:),1); % Will be the first time series loaded if stack was deactivated, or the only time series if only one station was loaded.
-  WPSD_to_plot = mean(WPSD_tab(IDs_to_process,:),1); % Will be the PSD of the first time series if stack was deactivated, or the PSD of the only time series if only one station was loaded.
+if (ismember(avgwpsds, [0, 1, 2]))
+  timeseries_to_plot = mean(raw_s(IDs_to_process, :), 1); % Will be the first time series loaded if stack was deactivated, or the only time series if only one station was loaded.
+  WPSD_to_plot = mean(WPSD_tab(IDs_to_process, :), 1); % Will be the PSD of the first time series if stack was deactivated, or the PSD of the only time series if only one station was loaded.
 
-  if(normalise_wpsd==1)
-    WPSD_to_plot=WPSD_to_plot/max(WPSD_to_plot);
+  if (normalise_wpsd == 1)
+    WPSD_to_plot = WPSD_to_plot / max(WPSD_to_plot);
   end
 
   figure();
@@ -140,83 +139,83 @@ if(ismember(avgwpsds,[0,1,2]))
   xlim([time(1), time(end)]);
   xlabel("$t$ (s)"); ylabel(timeseries_txt);
   title(timeseries_txt);
-  set(gca, 'TickLabelInterpreter','latex');
+  set(gca, 'TickLabelInterpreter', 'latex');
 
   figure();
   loglog(WPSD_f, WPSD_to_plot);
   xlim([WPSD_f(1), WPSD_f(end)]);
   xlabel("$f$ (Hz)"); ylabel(WPSD_txt);
   title(WPSD_txt);
-  set(gca, 'TickLabelInterpreter','latex');
+  set(gca, 'TickLabelInterpreter', 'latex');
   grid;
 
-  disp(sprintf("Amplitude of signal: %1.6e",max(timeseries_to_plot)-min(timeseries_to_plot)));
-elseif(avgwpsds==3)
+  disp(sprintf("[", mfilename, "] Amplitude of signal: %1.6e",max(timeseries_to_plot)-min(timeseries_to_plot)));
+elseif (avgwpsds == 3)
   figure();
-  colours=jet(numel(IDs_to_process));
-  for i=IDs_to_process
+  colours = jet(numel(IDs_to_process));
+  for i = IDs_to_process
     if (strcmp(coord_units, 'km'))
-      PSDName = strcat('S', num2str(istattab(i)), ', $(x,z)=(', num2str(xstattab(istattab(i)) / 1000), ',', num2str(ystattab(istattab(i)) / 1000), "$) ",coord_units);
+      PSDName = strcat('S', num2str(istattab(i)), ', $(x,z)=(', num2str(xstattab(istattab(i)) / 1000), ',', num2str(ystattab(istattab(i)) / 1000), "$) ", coord_units);
     elseif (strcmp(coord_units, 'm'))
-      PSDName = strcat('S', num2str(istattab(i)), ', $(x,z)=(', num2str(xstattab(istattab(i))), ',', num2str(ystattab(istattab(i))), ")$ ",coord_units);
+      PSDName = strcat('S', num2str(istattab(i)), ', $(x,z)=(', num2str(xstattab(istattab(i))), ',', num2str(ystattab(istattab(i))), ")$ ", coord_units);
     else
-      error(['coord_units = ', coord_units, 'not implemented.']);
+      error(['[', mfilename, ', ERROR] coord_units = ', coord_units, 'not implemented.']);
     end
-    loglog(WPSD_f, WPSD_tab(i,:), 'displayname',PSDName,'color',colours(i,:));
+    loglog(WPSD_f, WPSD_tab(i, :), 'displayname', PSDName, 'color', colours(i, :));
     hold on;
   end
   legend('location', 'best');
   xlim([WPSD_f(1), WPSD_f(end)]);
   xlabel("$f$ (Hz)"); ylabel(WPSD_txt);
   title(WPSD_txt);
-  set(gca, 'TickLabelInterpreter','latex');
+  set(gca, 'TickLabelInterpreter', 'latex');
   grid;
-elseif(avgwpsds==4)
-  distancechoice=-1;
-  while(~ismember(distancechoice,[1,2,3,4]))
-    distancechoice=input('  Distance choice? (1 for x, 2 for |x|, 3 for z, 4 for d) > ');
+elseif (avgwpsds == 4)
+  distancechoice = - 1;
+  while (~ ismember(distancechoice, [1, 2, 3, 4]))
+    distancechoice = input(['[', mfilename, '] Distance choice? (1 for x, 2 for |x|, 3 for z, 4 for d) > ']);
   end
   figure();
-  renorm_for_unit=-1;
+  renorm_for_unit = - 1;
   if (strcmp(coord_units, 'km'))
-    renorm_for_unit=1000;
+    renorm_for_unit = 1000;
   elseif (strcmp(coord_units, 'm'))
-    renorm_for_unit=1;
+    renorm_for_unit = 1;
   else
-    error(['coord_units = ', coord_units, 'not implemented.']);
+    error(['[', mfilename, ', ERROR] coord_units = ', coord_units, 'not implemented.']);
   end
   switch distancechoice
     case 1
-      SURFx=xstattab(istattab(IDs_to_process))/renorm_for_unit; dist_symbol='x';
+      SURFx = xstattab(istattab(IDs_to_process)) / renorm_for_unit; dist_symbol = 'x';
     case 2
-      SURFx=abs(xstattab(istattab(IDs_to_process)))/renorm_for_unit; dist_symbol='|x|';
+      SURFx = abs(xstattab(istattab(IDs_to_process))) / renorm_for_unit; dist_symbol = '|x|';
     case 3
-      SURFx=ystattab(istattab(IDs_to_process))/renorm_for_unit; dist_symbol='z';
+      SURFx = ystattab(istattab(IDs_to_process)) / renorm_for_unit; dist_symbol = 'z';
     case 4
-      SURFx=dist_to_sources(istattab(IDs_to_process))/renorm_for_unit; dist_symbol='d';
+      SURFx = dist_to_sources(istattab(IDs_to_process)) / renorm_for_unit; dist_symbol = 'd';
   end
-  SURFy=WPSD_f;
-  [SURFX,SURFY]=meshgrid(SURFx,SURFy);
-  surf(SURFX,SURFY,log10(WPSD_tab'));
+  SURFy = WPSD_f;
+  [SURFX, SURFY] = meshgrid(SURFx, SURFy);
+  surf(SURFX, SURFY, log10(WPSD_tab'));
   shading interp;
   xlim([min(SURFx), max(SURFx)]);
   ylim([min(SURFy), max(SURFy)]);
-  set(gca,'yscale','log');
-  xlabel(['$',dist_symbol,'$ (',coord_units,')']);
+  set(gca, 'yscale', 'log');
+  xlabel(['$', dist_symbol, '$ (', coord_units, ')']);
   ylabel(['$f$ (Hz)']);
-%   title(strcat("$\log($",WPSD_txt,"$)$"));
+  %   title(strcat("$\log($",WPSD_txt,"$)$"));
   title(WPSD_txt);
-  set(gca, 'TickLabelInterpreter','latex');
-  view([0,0,1]);
+  set(gca, 'TickLabelInterpreter', 'latex');
+  view([0, 0, 1]);
   grid;
-  cb=colorbar;
-  set(cb, 'TickLabelInterpreter','latex');
-  set(cb,'ticklabels',split(sprintf('$10^{%g}$ ',get(cb, 'ticks'))));
+  cb = colorbar;
+  set(cb, 'TickLabelInterpreter', 'latex');
+  set(cb, 'ticklabels', split(sprintf('$10^{%g}$ ', get(cb, 'ticks'))));
 else
-  error(['[',mfilename,', ERROR] bad value for avgwpsds.']);
+  error(['[', mfilename, ', ERROR] [', mfilename, ', ERROR] bad value for avgwpsds.']);
 end
 
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Clear variables.             %
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 clear('select_time_l', 'select_time_u', 'signal_name', 'unit');
