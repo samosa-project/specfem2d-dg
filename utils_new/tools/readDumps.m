1ea6f61233c70122ed80c3f31a47a4fd1ecbe44d
Léo Martire <30411754+leomartire@users.noreply.github.com>
2019-06-06 16:36:13 +0200

    [SCRIPTS] Scripts update (190606).

--- utils_new/tools/readDumps.m
+++ utils_new/tools/readDumps.m
@@ -41,7 +41,21 @@ function [X,Y,V] = readDumps(OFD, IT, verbose)
 %       size(XY)
     else
   %     disp(['''',wavefield_file_name,''' is a value file, loading it.']);
-      if( regexp(wavefield_file_name,['0',num2str(IT),'_']) | regexp(wavefield_file_name,['d',num2str(IT),'_']))
+  
+      itname_fills_number_span = regexp(wavefield_file_name,['d',num2str(IT),'_']);
+      if(isempty(itname_fills_number_span))
+        itname_fills_number_span = 0;
+      end
+      itname_has_zero_right_before = regexp(wavefield_file_name,['0',num2str(IT),'_']);
+      if(isempty(itname_has_zero_right_before))
+        itname_has_zero_right_before = 0;
+      end
+      itname_has_something_else_right_before_and_before_text = regexp(wavefield_file_name,['0+[1-9]0*',num2str(IT),'_']);
+      if(isempty(itname_has_something_else_right_before_and_before_text))
+        itname_has_something_else_right_before_and_before_text = 0;
+      end
+      
+      if( itname_fills_number_span | (itname_has_zero_right_before & not(itname_has_something_else_right_before_and_before_text)) )
         inp = importdata(wavefield_file_path);
         if(verbose)
           disp(['value file ''',wavefield_file_name,''' has right iteration ID, needs to be loaded, will be, contains [',num2str(size(inp)),'] values.']);
