5713afe7f2d23898a34e8160d4d9849ad1065900
Léo Martire <30411754+leomartire@users.noreply.github.com>
2019-06-11 10:18:33 +0200

    [SRC] Added default value for newly-introduced parameter 'IONOSPHERIC_COUPLING': in case it is not found in parfile, set it to .false. to deactivate ionospheric coupling. Also, leave it to .false. (do not try to read it) if DG is not activated (USE_DISCONTINUOUS_METHOD=.false.), as it is not implented otherwise (safeguard).

--- src/meshfem2D/read_parameter_file.F90
+++ src/meshfem2D/read_parameter_file.F90
@@ -171,8 +171,14 @@
   call read_value_logical_p(CONSTRAIN_HYDROSTATIC, 'solver.CONSTRAIN_HYDROSTATIC')
   if (err_occurred() /= 0) stop 'error reading parameter CONSTRAIN_HYDROSTATIC in Par_file'
   
-  call read_value_logical_p(IONOSPHERIC_COUPLING, 'solver.IONOSPHERIC_COUPLING')
-  if (err_occurred() /= 0) stop 'error reading parameter IONOSPHERIC_COUPLING in Par_file'
+  !call read_value_logical_p(IONOSPHERIC_COUPLING, 'solver.IONOSPHERIC_COUPLING')
+  !if (err_occurred() /= 0) stop 'error reading parameter IONOSPHERIC_COUPLING in Par_file'
+  IONOSPHERIC_COUPLING = .false. ! Default value.
+  if(USE_DISCONTINUOUS_METHOD) then
+    call read_value_logical_p(IONOSPHERIC_COUPLING, 'solver.IONOSPHERIC_COUPLING')
+    if (err_occurred() /= 0) write(*,*) 'Error reading parameter IONOSPHERIC_COUPLING in Par_file. '&
+                                        'Setting to .false., do not worry.'
+  endif
   
   call read_value_logical_p(USE_ISOTHERMAL_MODEL, 'solver.USE_ISOTHERMAL_MODEL')
   if (err_occurred() /= 0) stop 'error reading parameter USE_ISOTHERMAL_MODEL in Par_file'
