42f2eed5240e265538d8178c364dc2b810454cf2
Léo Martire <30411754+leomartire@users.noreply.github.com>
2019-08-06 09:24:07 +0200

    [SRC, LNS] Fixed some comments' length to avoid Intel compiler warnings.

--- src/specfem2D/specfem2D_par_LNS.f90
+++ src/specfem2D/specfem2D_par_LNS.f90
@@ -10,10 +10,13 @@ module specfem_par_LNS
   ! Switch enabling the use of LNS instead of FNS.
   logical :: USE_LNS
   
-  ! Switch to activate the use of the "desintegration method" for gradient computation methods, and to desactivate to use the SEM definition of the gradient. ! Warning: LNS_switch_gradient = .true. is not yet fully implemented.
+  ! Switch to activate the use of the "desintegration method" for gradient computation methods, and to desactivate to use the
+  ! SEM definition of the gradient. ! Warning: LNS_switch_gradient = .true. is not yet fully implemented.
   logical, parameter :: LNS_switch_gradient = .false.
   
-  ! General switch being true if (maxval(LNS_mu) > 0. .OR. maxval(LNS_eta) > 0. .OR. maxval(LNS_kappa) > 0.) and false if not. See compute_forces_acoustic_LNS_calling_routine. In the latter case, enables faster verification and thus faster skipping of some parts of the code.
+  ! General switch being true if (maxval(LNS_mu) > 0. .OR. maxval(LNS_eta) > 0. .OR. maxval(LNS_kappa) > 0.) and false if not.
+  ! See compute_forces_acoustic_LNS_calling_routine. In the latter case, enables faster verification and thus faster skipping
+  ! of some parts of the code.
   logical :: LNS_viscous
   ! ------------------------------------------------------------ !
   
@@ -23,8 +26,8 @@ module specfem_par_LNS
   ! Constants (TODO maybe move to constants.h.in).
   real(kind=CUSTOM_REAL), parameter :: R_ADIAB = 8.3144598
   
-  ! LSRK coefficients.
-  real(kind=CUSTOM_REAL), dimension(:), allocatable :: LNS_scheme_A, LNS_scheme_B, LNS_scheme_C ! Size should be stage_time_scheme.
+  ! LSRK coefficients. ! Size should be stage_time_scheme.
+  real(kind=CUSTOM_REAL), dimension(:), allocatable :: LNS_scheme_A, LNS_scheme_B, LNS_scheme_C
   ! ------------------------------------------------------------ !
   
   ! ---------------------------- !
@@ -37,11 +40,13 @@ module specfem_par_LNS
   ! ---------------------------- !
   ! Printing parameters.
   ! ---------------------------- !
-  integer(kind=selected_int_kind(2)), parameter :: LNS_VERBOSE = 99 ! Verbosity parameter. Min/Maximum values: [-10^2+1=-99, 10^2-1=99].
+  ! Verbosity parameter. Min/Maximum values: [-10^2+1=-99, 10^2-1=99].
+  integer(kind=selected_int_kind(2)), parameter :: LNS_VERBOSE = 99
   ! LNS_VERBOSE>= 1: printing iteration, stages, and local times, every LNS_MODPRINT iterations
   ! LNS_VERBOSE>=51: printing min/max values for each constitutive variable on CPU 0 every LNS_MODPRINT iterations
   
-  ! Integer used in modulo for condition on printing min/max values for each constitutive variable on CPU 0 every 100 iterations. To be added in parfile later.
+  ! Integer used in modulo for condition on printing min/max values for each constitutive variable on CPU 0 every 100
+  ! iterations. To be added in parfile later.
   integer, parameter :: LNS_MODPRINT = 500
   ! ------------------------------------------------------------ !
   
@@ -59,7 +64,10 @@ module specfem_par_LNS
   ! Auxiliary initial state quantities.
   real(kind=CUSTOM_REAL), dimension(:),     allocatable :: LNS_p0, LNS_T0
   real(kind=CUSTOM_REAL), dimension(:,:,:), allocatable :: nabla_v0 ! Gradient of initial velocity.
-  real(kind=CUSTOM_REAL), dimension(:,:), allocatable :: sigma_v_0 ! Initial viscous stress tensor. Symmetric, thus only need to save few (NVALSIGMA) entries. In 2D : (1,:) index must correspond to the index (1, 1) of the actual tensor, (2, :) <-> (1, 2) and (2, 1) of the actual tensor, and (3, :) <-> (2, 2) of the actual tensor.
+  ! Initial viscous stress tensor. Symmetric, thus only need to save few (NVALSIGMA) entries. In 2D : (1,:) index must
+  ! correspond to the index (1, 1) of the actual tensor, (2, :) <-> (1, 2) and (2, 1) of the actual tensor,
+  ! and (3, :) <-> (2, 2) of the actual tensor.
+  real(kind=CUSTOM_REAL), dimension(:,:), allocatable :: sigma_v_0
   
   ! State.
   real(kind=CUSTOM_REAL), dimension(:),   allocatable :: LNS_drho, LNS_dE
@@ -67,9 +75,11 @@ module specfem_par_LNS
   
   ! Auxiliary state quantities.
   real(kind=CUSTOM_REAL), dimension(:),   allocatable :: LNS_dp, LNS_dT
-  real(kind=CUSTOM_REAL), dimension(:,:), allocatable :: LNS_dv, LNS_dm ! Velocity perturbation, and momentum (1st order) perturbation.
+  ! Velocity perturbation, and momentum (1st order) perturbation.
+  real(kind=CUSTOM_REAL), dimension(:,:), allocatable :: LNS_dv, LNS_dm
   real(kind=CUSTOM_REAL), dimension(:,:), allocatable :: nabla_dT ! Gradient of temperature perturbation.
-  real(kind=CUSTOM_REAL), dimension(:,:), allocatable :: sigma_dv ! Viscous stress tensor perturbation. Symmetric, thus only need to save few entries (see sigma_v_0).
+  ! Viscous stress tensor perturbation. Symmetric, thus only need to save few entries (see sigma_v_0).
+  real(kind=CUSTOM_REAL), dimension(:,:), allocatable :: sigma_dv
   ! ------------------------------------------------------------ !
   
   ! ---------------------------- !
@@ -99,22 +109,35 @@ module specfem_par_LNS
   ! Pretty much all these arrays are allocated in 'prepare_timerun_pml.f90'.
   ! /!\ PMLs are still under developement.
   integer :: nglob_PML ! Number of PML points (spatial duplicates included). Computed in 'pml_init.f90'.
-  !real(kind=CUSTOM_REAL), dimension(:), allocatable :: rmass_inverse_acoustic_LNS_PML ! Inverse mass matrix. Size allocated should be (nglob_PML).
+  ! Inverse mass matrix. Size allocated should be (nglob_PML).
+  !real(kind=CUSTOM_REAL), dimension(:), allocatable :: rmass_inverse_acoustic_LNS_PML
   integer, dimension(:,:,:), allocatable :: ibool_LNS_PML ! Same as ibool_DG (see 'specfem2D_par'), but for PML only.
-  !real(kind=CUSTOM_REAL), dimension(:,:,:,:), allocatable :: LNS_PML_drho, LNS_PML_dE ! Size allocated should be (NADE, NGLLX, NGLLZ, nspec_PML).
-  !real(kind=CUSTOM_REAL), dimension(:,:,:,:,:), allocatable :: LNS_PML_rho0dv ! Size allocated should be (NADE, NDIM, NGLLX, NGLLZ, nspec_PML).
+  ! Size allocated should be (NADE, NGLLX, NGLLZ, nspec_PML).
+  !real(kind=CUSTOM_REAL), dimension(:,:,:,:), allocatable :: LNS_PML_drho, LNS_PML_dE
+  ! Size allocated should be (NADE, NDIM, NGLLX, NGLLZ, nspec_PML).
+  !real(kind=CUSTOM_REAL), dimension(:,:,:,:,:), allocatable :: LNS_PML_rho0dv
   !real(kind=CUSTOM_REAL), dimension(:,:,:,:), allocatable :: RHS_PML_drho,aux_PML_drho, RHS_PML_dE,aux_PML_dE
   !real(kind=CUSTOM_REAL), dimension(:,:,:,:,:), allocatable :: RHS_PML_rho0dv, aux_PML_rho0dv
-  !real(kind=CUSTOM_REAL), dimension(:,:), allocatable :: LNS_PML_drho, LNS_PML_dE ! Size allocated should be (NADE, NGLLX*NGLLZ*nspec_PML).
-  !real(kind=CUSTOM_REAL), dimension(:,:,:), allocatable :: LNS_PML_rho0dv ! Size allocated should be (NADE, NDIM, NGLLX*NGLLZ*nspec_PML).
+  ! Size allocated should be (NADE, NGLLX*NGLLZ*nspec_PML).
+  !real(kind=CUSTOM_REAL), dimension(:,:), allocatable :: LNS_PML_drho, LNS_PML_dE
+  ! Size allocated should be (NADE, NDIM, NGLLX*NGLLZ*nspec_PML).
+  !real(kind=CUSTOM_REAL), dimension(:,:,:), allocatable :: LNS_PML_rho0dv
   !real(kind=CUSTOM_REAL), dimension(:,:), allocatable :: RHS_PML_drho,aux_PML_drho, RHS_PML_dE,aux_PML_dE
   !real(kind=CUSTOM_REAL), dimension(:,:,:), allocatable :: RHS_PML_rho0dv, aux_PML_rho0dv
   integer, parameter :: LNS_PML_NAUX=6 ! Number of auxiliary variables per constitutive variable for PMLs.
-  real(kind=CUSTOM_REAL), dimension(:,:,:), allocatable :: LNS_PML, LNS_PML_RHS, LNS_PML_aux ! Dimensions: 1<-> # constitutive variables, 2<-> # auxiliary variables, 3<-> #PML mesh points.
-  real(kind=CUSTOM_REAL), dimension(:,:,:,:), allocatable :: LNS_PML_kapp ! Constant coefficient in the stretching s. Size allocated should be (NDIM, NGLLX, NGLLZ, nspec_PML).
-  real(kind=CUSTOM_REAL), dimension(:,:,:,:), allocatable :: LNS_PML_alpha ! Coefficient in front of the auxiliary variables. For classical formulation, only 2=NDIM ADE are to be solved for each variable, hence the first dimension.
-  real(kind=CUSTOM_REAL), dimension(:,:,:), allocatable :: LNS_PML_a0 ! Coefficient in front of the \delta, that is in front of the q in the updated strong form. Size allocated should be (NGLLX,NGLLZ,nspec_PML) in order to save memory.
-  real(kind=CUSTOM_REAL), dimension(:,:,:,:), allocatable :: LNS_PML_b, LNS_PML_d ! Coefficient in front of each auxiliary variable (ADEs). For classical formulation, only 2=NDIM ADE are to be solved for each variable, hence the first dimension.
+  ! Dimensions: 1<-> # constitutive variables, 2<-> # auxiliary variables, 3<-> #PML mesh points.
+  real(kind=CUSTOM_REAL), dimension(:,:,:), allocatable :: LNS_PML, LNS_PML_RHS, LNS_PML_aux
+  ! Constant coefficient in the stretching s. Size allocated should be (NDIM, NGLLX, NGLLZ, nspec_PML).
+  real(kind=CUSTOM_REAL), dimension(:,:,:,:), allocatable :: LNS_PML_kapp
+  ! Coefficient in front of the auxiliary variables. For classical formulation, only 2=NDIM ADE are to be solved for each
+  ! variable, hence the first dimension.
+  real(kind=CUSTOM_REAL), dimension(:,:,:,:), allocatable :: LNS_PML_alpha
+  ! Coefficient in front of the \delta, that is in front of the q in the updated strong form. Size allocated should be
+  ! (NGLLX,NGLLZ,nspec_PML) in order to save memory.
+  real(kind=CUSTOM_REAL), dimension(:,:,:), allocatable :: LNS_PML_a0
+  ! Coefficient in front of each auxiliary variable (ADEs). For classical formulation, only 2=NDIM ADE are to be solved for
+  ! each variable, hence the first dimension.
+  real(kind=CUSTOM_REAL), dimension(:,:,:,:), allocatable :: LNS_PML_b, LNS_PML_d
   ! ------------------------------------------------------------ !
   
   ! ---------------------------- !
@@ -130,9 +153,10 @@ module specfem_par_LNS
   ! Dummy variables.
   ! ---------------------------- !
   ! Those are useful for using the 'DG' extension routines without re-coding them.
-  real(kind=CUSTOM_REAL), dimension(:),     allocatable :: LNS_dummy_1d ! Dummy variables are not optimal, but prevent from duplicating subroutines.
-  real(kind=CUSTOM_REAL), dimension(:,:),   allocatable :: LNS_dummy_2d ! Dummy variables are not optimal, but prevent from duplicating subroutines.
-  real(kind=CUSTOM_REAL), dimension(:,:,:), allocatable :: LNS_dummy_3d ! Dummy variables are not optimal, but prevent from duplicating subroutines.
+  ! Dummy variables are not optimal, but prevent from duplicating subroutines.
+  real(kind=CUSTOM_REAL), dimension(:),     allocatable :: LNS_dummy_1d
+  real(kind=CUSTOM_REAL), dimension(:,:),   allocatable :: LNS_dummy_2d
+  real(kind=CUSTOM_REAL), dimension(:,:,:), allocatable :: LNS_dummy_3d
   ! ------------------------------------------------------------ !
   
   ! ---------------------------- !
@@ -181,7 +205,8 @@ module specfem_par_LNS
 !  ! ------------------------------------------------------------ !
 !  ! RusanovFlux                                                  !
 !  ! ------------------------------------------------------------ !
-!  ! Implements the Rusanov approximation for the computation of the flux across an interface where the following Riemann problem is posed:
+!  ! Implements the Rusanov approximation for the computation of the flux across an interface where the following Riemann
+!  ! problem is posed:
 !  !   \partial_t q + \nabla\cdot\Sigma = S
 !  function RusanovFlux(Sigma_M, Sigma_P, q_M, q_P, normal_vec, exact_interface_flux, lambda) result(flux)
 !    use constants, only: CUSTOM_REAL, NDIM
