404a4b1611cfecd94e78be43931a7f31418dc27d
Léo Martire <30411754+leomartire@users.noreply.github.com>
2020-03-12 11:19:52 +0100

    [SCRIPTS, LNS] Update to background loading scripts.

--- utils_new/lns_background_models/write_bg_model.m
+++ utils_new/lns_background_models/write_bg_model.m
@@ -1,9 +1,14 @@
 function [] = write_bg_model(OFD, IT)
   nb_significantdigits = 16;
-  output_file = '/home/l.martire/Documents/SPECFEM/specfem-dg-master/EXAMPLES/test_lns_custom_wavefield_fromdg/background_model.dat';
   do_interp = 0;
-  do_interp_NINTERPX = 100;
+  do_interp_NINTERPX = 4;
   do_interp_NINTERPZ = do_interp_NINTERPX;
+  
+  output_file = '/home/l.martire/Documents/SPECFEM/specfem-dg-master/EXAMPLES/test_lns_custom_wavefield_fromdg/background_model.dat';
+%   output_file = '/home/l.martire/Documents/SPECFEM/specfem-dg-master/EXAMPLES/test_lns_custom_wavefield_4elements/background_model.dat';
+
+  tweaks = 1;
+  factor_dp = 2e6; % increase the pressure perturbation by a factor
   
   % get quantities to put in model
   [order, lab] = order_bg_model();
@@ -37,6 +42,17 @@ function [] = write_bg_model(OFD, IT)
     VAL = reshape(Zi,ninterp,1);
   end
   
+  % lexicographic order, for readability
+  [~, isort] = sortrows([X, Z]);
+  X = X(isort);
+  Z = Z(isort);
+  VAL = VAL(isort);
+  
+  % handmade tweaks for tests
+  if(tweaks)
+    VAL = min(VAL) + factor_dp*(VAL-min(VAL));
+  end
+  
 %   X = X(1:10);
 %   Y = Y(1:10);
 %   V = V(1:10);
@@ -57,7 +73,12 @@ function [] = write_bg_model(OFD, IT)
   
 %   % visualisation
 %   figure(); spy(ROWS); daspect([1,numel(X)/nb_qty,1]); xticks(1:nb_qty); xticklabels(order);
+%   toplot = VAL;
+  toplot = sqrt(gam*VAL/rho0);
+  [Xi, Zi, VALi] = interpDumps(X, Z, toplot, 1000, 1000); fh = figure(); pcolor(Xi, Zi, VALi); shading interp; colormaps_fromPython('seismic', 1); colorbar;
+  pause
   
+  % Open file and write in it.
   foutput = fopen(output_file, 'w');
   
   % header
@@ -70,6 +91,7 @@ function [] = write_bg_model(OFD, IT)
   fprintf(foutput, format_line, ROWS');
   
   fclose('all');
+  disp(['[',mfilename,'] Finished writing background model to file (''',output_file,''').']);
   
 end
 
