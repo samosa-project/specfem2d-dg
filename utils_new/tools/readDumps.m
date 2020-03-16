5685aa0ab4da3693a8c5b600b3709c13bda1be1d
Léo Martire <30411754+leomartire@users.noreply.github.com>
2020-03-16 11:21:31 +0100

    [SCRIPTS] Updated 'utils_new/tools/readDumps.m' to enable reading binary dumps.

--- utils_new/tools/readDumps.m
+++ utils_new/tools/readDumps.m
@@ -12,9 +12,20 @@ function [X, Y, V, imagetype_wavefield_dumps] = readDumps(OFD, IT, verbose)
   if(not(strcmp(OFD(end),filesep)))
     OFD = [OFD,filesep];
   end
-
+  
+  parfile=[OFD,'input_parfile'];
+  
+  % are we indeed dumping dumps?
+  output_wavefield_dumps = readExampleFiles_extractParam(parfile, 'output_wavefield_dumps', 'bool');
+  if(not(output_wavefield_dumps))
+    error(['[',mfilename,', ERROR] Required OUTPUT_FILES (''',OFD,''') is not dumping dumps (as read from parfile ''',parfile,''').']);
+  end
+  
+  % binary or ascii
+  use_binary_for_wavefield_dumps = readExampleFiles_extractParam(parfile, 'use_binary_for_wavefield_dumps', 'bool');
+  
   % try identifying NPROC, for safety checks later.
-  NPROC = readExampleFiles_extractParam([OFD,'input_parfile'],'NPROC','int');
+  NPROC = readExampleFiles_extractParam(parfile,'NPROC','int');
   
   % check dump type
   imagetype_wavefield_dumps = readExampleFiles_extractParam([OFD,'input_parfile'],'imagetype_wavefield_dumps','int');
@@ -30,33 +41,65 @@ function [X, Y, V, imagetype_wavefield_dumps] = readDumps(OFD, IT, verbose)
     disp(['[] imagetype_wavefield_dumps = ',num2str(imagetype_wavefield_dumps), ', ',num2str(nvalues_dumped),' to be read per mesh point']);
   end
 
-  wavefield_dump_wildcard = [OFD, 'wavefield*.txt'];
+  wvflddmp_wildcard_txt = [OFD, 'wavefield*.txt'];
+  wvflddmp_wildcard_bin = [OFD, 'wavefield*.bin'];
 
   % dir(wavefield_dump_wildcard)
-  wavefield_files = dir(wavefield_dump_wildcard);
-
+  wvflddmp_txtfiles = dir(wvflddmp_wildcard_txt);
+  wvflddmp_binfiles = dir(wvflddmp_wildcard_bin);
+  
+  if(use_binary_for_wavefield_dumps)
+    % If binary, grab numbers of points. They should be the only .txt files anyhow.
+    if(numel(wvflddmp_txtfiles)==NPROC)
+      % ok
+    else
+      error(['[] In this configuration, the number of .txt files should match NPROC from parfile.']);
+    end
+    bin_nglob = zeros(NPROC, 1);
+    for f=1:numel(wvflddmp_txtfiles)
+      bin_nglob(f) = importdata([wvflddmp_txtfiles(f).folder,filesep,wvflddmp_txtfiles(f).name]);
+    end
+    
+    toRead = wvflddmp_binfiles;
+  else
+    % If ASCII.
+    toRead = wvflddmp_txtfiles;
+  end
 
   XY = [];
   V = [];
-  for i=1:numel(wavefield_files)
-    wavefield_file_name = wavefield_files(i).name;
-    wavefield_file_path = [wavefield_files(i).folder,filesep,wavefield_file_name];
+  for i=1:numel(toRead)
+    wavefield_file_name = toRead(i).name;
+    wavefield_file_path = [toRead(i).folder,filesep,wavefield_file_name];
 
-  %   disp([' >>>>>>>> Reading ''',wavefield_file_name,'''. <<<<<<<<']); % info
+    disp(['[',mfilename,'] Reading ''',wavefield_file_name,'''.']);
+%     pause
 
     if(regexp(wavefield_file_name,'grid_value_of_nglob'))
-      % nglob, do not load it (maybe usefule for checking)
+      % If binary, no such file exist. If ASCII, we do not care (may be useful for checking). Skip.
   %     disp('  This is a grid file.');
     elseif(regexp(wavefield_file_name,'grid_for_dumps'))
-      % grid, load it;
-      inp = importdata(wavefield_file_path);
+      % This is a grid file, load it;
       if(verbose)
-        disp(['grid  file ''',wavefield_file_name,''' needs to be loaded, will be, contains [',num2str(size(inp)),'] values.']);
+        disp(['grid  file ''',wavefield_file_name,''' needs to be loaded.']);
+%         pause
       end
-      XY = [XY; importdata(wavefield_file_path)];
-%       size(XY)
+      if(use_binary_for_wavefield_dumps)
+        % binary loading
+        binfile = fopen(wavefield_file_path, 'r');
+        machinefmt = 'single'; % This is determined by the 'write' command in 'write_wavefields_dumps.f90'.
+        inp = fread(binfile, machinefmt); fclose(binfile);
+        inp = reshape(inp, [2, numel(inp)/2])'; % Reshape HAS TO be like this, because binary file lists points as 'x,y,x,y,x,y' and hence first loading of inp stores them as [x;y;x;y;x;y]. N.B.: 'numel/2' would crash if wrong number of singles loaded.
+      else
+        % ascii loading
+        inp = importdata(wavefield_file_path);
+      end
+      XY = [XY; inp];
     else
-  %     disp(['''',wavefield_file_name,''' is a value file, loading it.']);
+      
+      % This should be a file containing values.
+      disp(['[',mfilename,'] ''',wavefield_file_name,''' should be a value file, checking it has right shape, is of right iteration, and then loading it.']);
+%       pause
   
       itname_fills_number_span = regexp(wavefield_file_name,['d',num2str(IT),'_']);
       if(isempty(itname_fills_number_span))
@@ -72,14 +115,35 @@ function [X, Y, V, imagetype_wavefield_dumps] = readDumps(OFD, IT, verbose)
       end
       
       if( itname_fills_number_span | (itname_has_zero_right_before & not(itname_has_something_else_right_before_and_before_text)) )
-        inp = importdata(wavefield_file_path);
         if(verbose)
-          disp(['value file ''',wavefield_file_name,''' has right iteration ID, needs to be loaded, will be, contains [',num2str(size(inp)),'] values.']);
+          disp(['[',mfilename,'] Value file ''',wavefield_file_name,''' has right iteration ID, needs to be loaded.']);
+%           pause
         end
+        if(use_binary_for_wavefield_dumps)
+          % binary loading
+          binfile = fopen(wavefield_file_path, 'r');
+          machinefmt = 'single'; % This is determined by the 'write' command in 'write_wavefields_dumps.f90'.
+          inp = fread(binfile, machinefmt); fclose(binfile);
+          % identify which proc was that
+          curProc = str2double(regexp(regexp(wavefield_file_path,'_[0-9]+\.bin','match','once'),'[0-9]+','match','once'));
+          if(numel(inp)==bin_nglob(curProc+1))
+            % Loaded the right number of values, all is right.
+          else
+            error(['[',mfilename,', ERROR] This file is (supposedly) related to PROC n°',num2str(curProc),'. Hence, should have loaded ',num2str(bin_nglob(curProc+1)),' values, but only loaded ',num2str(numel(inp)),'.']);
+          end
+        else
+          % ascii loading
+          inp = importdata(wavefield_file_path);
+        end
+%         if(verbose)
+%           disp(['[',mfilename,'] Value file ''',wavefield_file_name,''' has right iteration ID, needs to be loaded, will be, contains [',num2str(size(inp)),'] values.']);
+%         end
         V = [V;inp];
 %         size(V)
       else
-  %       disp(['''',wavefield_file_name,''' is a value file with wrong iteration ID, do nothing.']);
+        if(verbose)
+          disp(['[',mfilename,'] Value file ''',wavefield_file_name,''' should not be loaded. Skipping.']);
+        end
       end
     end
   end
