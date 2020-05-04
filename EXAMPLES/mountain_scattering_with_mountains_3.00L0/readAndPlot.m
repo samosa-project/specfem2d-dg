f0c562cfa0b010952517e7d4b698feb2cbfa594c
Léo Martire <30411754+leomartire@users.noreply.github.com>
2020-05-04 13:46:20 +0200

    [EXAMPLES] updated scattering example

--- EXAMPLES/mountain_scattering_with_mountains_3.00L0/readAndPlot.m
+++ EXAMPLES/mountain_scattering_with_mountains_3.00L0/readAndPlot.m
@@ -2,15 +2,18 @@ clear all;
 close all;
 clc;
 
+addpath(genpath('/home/l.martire/Documents/SPECFEM/specfem-dg-master/utils_new'));
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Parameters.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % without_0__with033L_1__with300L_2 = 1; OF = 'OUTPUT_FILES_4213746_test'; IT = 45000; boxabsx = 18e3; boxy = [1500, 7.5e3];
 
 IT = 100000; boxabsx = 28e3; boxy = [1501, 15e3];
-% LTOPO_over_L0 = Inf; OF = 'OUTPUT_FILES_4214695_fns';
-% LTOPO_over_L0 = 0.33; OF = 'OUTPUT_FILES_4214696_fns';
-LTOPO_over_L0 = 3.00; OF = 'OUTPUT_FILES_4214697_fns';
+LTOPO_over_L0 = Inf; OF = 'OUTPUT_FILES_4214695_fns';
+% LTOPO_over_L0 = 0.331; OF = 'OUTPUT_FILES_4214696_fns';
+% LTOPO_over_L0 = 0.332; OF = 'OUTPUT_FILES_4219868_fns';
+% LTOPO_over_L0 = 3.00; OF = 'OUTPUT_FILES_4214697_fns';
 
 forceDGMesh = 0; % Force the use of the DG mesh for interpolation? Might be heavy, but is less wrong.
 dx = 20; dz = dx; % If forceDGMesh=0, wanted dx and dz for interpolation.
@@ -23,14 +26,18 @@ output_figures_folder = [thisFolder, filesep, 'FIGURES'];
 figfieldpath = [output_figures_folder, filesep, '',regexprep(num2str(LTOPO_over_L0),'\.','p'),'_field'];
 figspectpath = [output_figures_folder, filesep, '',regexprep(num2str(LTOPO_over_L0),'\.','p'),'_spect'];
 figspectdiffpath = [output_figures_folder, filesep, '',regexprep(num2str(LTOPO_over_L0),'\.','p'),'_spectdiff'];
+extToSave = {'eps'};
 
 switch(LTOPO_over_L0)
   case Inf
     OFDroot = '/home/l.martire/Documents/SPECFEM/specfem-dg-master/EXAMPLES/mountain_scattering_without_mountains';
     TIT_addendum = 'without topography';
-  case 0.33
+  case 0.331
     OFDroot = '/home/l.martire/Documents/SPECFEM/specfem-dg-master/EXAMPLES/mountain_scattering_with_mountains_0.33L0';
-    TIT_addendum = 'with $L_0/3$ topography';
+    TIT_addendum = 'with $L_0/3$ high topography';
+  case 0.332
+    OFDroot = '/home/l.martire/Documents/SPECFEM/specfem-dg-master/EXAMPLES/mountain_scattering_with_mountains_0.33L0_lower';
+    TIT_addendum = 'with $L_0/3$ low topography';
   case 3
     OFDroot = '/home/l.martire/Documents/SPECFEM/specfem-dg-master/EXAMPLES/mountain_scattering_with_mountains_3.00L0';
     TIT_addendum = 'with $3L_0$ topography';
@@ -85,7 +92,8 @@ disp(['[',mfilename,'] Interpolating.']);
 % Figure.
 TIT = ['Pressure Field ',TIT_addendum];
 CBYLAB_PRESPEC = ['$\Delta P$ [Pa]'];
-fig_field = figure('units','normalized','outerposition',[0,0,1,1]);
+fig_field = figure('units','normalized','outerposition',[0,0,1,0.65]);
+tightAxes = tight_subplot(1, 1, [0,0], [0.18,0.12], [0.06, 0.03]);
 pcolor(Xi/1e3, Yi/1e3, Vi.pre); hold on;
 daspect([1,1,1]);
 hcb = colorbar(); ylabel(hcb, CBYLAB_PRESPEC, 'interpreter', 'latex');
@@ -93,7 +101,7 @@ caxis([-1,1]*max(abs(Vi.pre(:))));
 colormaps_fromPython('seismic', 1);
 xlabel(['$x$ [km]']); ylabel(['$z$ [km]']);
 title(TIT);
-customSaveFig(fig_field, [figfieldpath], {'png', 'eps'}, 9999);
+customSaveFig(fig_field, [figfieldpath], extToSave, 9999);
 
 % Compute FFT.
 x = unique(Xi); z = unique(Yi);
@@ -125,7 +133,7 @@ hcb = colorbar(); ylabel(hcb, CBYLAB_PRESPEC, 'interpreter', 'latex');
 colormaps_fromPython('inferno', 1); caxis(max(toPlot(:))+[-2,0]);
 xlabel(XLAB); ylabel(YLAB); title(TIT);
 legend(h_kxkzth, 'location', 'northeast');
-customSaveFig(fig_spect, [figspectpath], {'png', 'eps'}, 9999);
+customSaveFig(fig_spect, [figspectpath], extToSave, 9999);
 
 % Compare.
 if(not(LTOPO_over_L0==Inf))
@@ -140,13 +148,14 @@ if(not(LTOPO_over_L0==Inf))
     CBYLAB = ['signed difference of ',CBYLAB_PRESPEC];
     pcolor(kx, kz, toPlot); hold on;
     set(gca, 'xscale', 'log', 'yscale', 'log');
+    daspect([1,1,1]);
     hcb = colorbar(); ylabel(hcb, CBYLAB, 'interpreter', 'latex');
     % adjust colormap
     maxneg = abs(min(toPlot(:))); maxpos = max(toPlot(:)); thresh = max([maxneg, maxpos])-1;
     colormaps_custom([-maxneg, [-1,-0.8,0,0.8,1]*thresh, maxpos], [[0,0,1].*[0.25,1]';[0.8,0.8,1];[1,1,1];[1,0.8,0.8];[1,0,0].*[1,0.25]'], 1);
     caxis([-maxneg,maxpos]);
     xlabel(XLAB); ylabel(YLAB); title(TIT);
-    customSaveFig(fig_spectdiff, [figspectdiffpath], {'png', 'eps'}, 9999);
+    customSaveFig(fig_spectdiff, [figspectdiffpath], extToSave, 9999);
   else
     error('base file does not exist');
   end
