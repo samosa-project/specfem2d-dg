c5d05009c2dd4fbec5f33d33fb79dfd953856314
Léo Martire <30411754+leomartire@users.noreply.github.com>
2020-08-20 13:37:44 +0200

    [EXAMPLES] Validation fluid-solid coupling: removed old test case, which was bad anyway.

--- EXAMPLES/validation_fluid_solid_coupling/ReflexionTransmissionCoefs.m
+++
@@ -1,7 +0,0 @@
-function [R, T] = ReflexionTransmissionCoefs(Z_1, Z_2, i_1, i_2)
-  % Coefficients for P-wave transmission-reflection, from Fresnel theory.
-  % E.g., Born, M., & Wolf, E. (1981). Principles of Optics: Electromagnetic Theory of Propagation Interference and Diffraction of Light (6th ed.). Cambridge University Press.
-  
-  R = (Z_2*cos(i_1) - Z_1*cos(i_2)) / (Z_2*cos(i_1) + Z_1*cos(i_2));
-  T = 2*Z_1*cos(i_1) / (Z_2*cos(i_1) + Z_1*cos(i_2));
-end
--- EXAMPLES/validation_fluid_solid_coupling/snells.m
+++
@@ -1,3 +0,0 @@
-function [theta_out] = snells(v_inc, v_out, theta_inc)
-  theta_out = asin( (v_out/v_inc)*sin(theta_inc) );
-end
