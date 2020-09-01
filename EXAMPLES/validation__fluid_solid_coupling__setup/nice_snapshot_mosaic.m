804efdf4f6cab190c109181fe5068f9dfd24e21d
Léo Martire <30411754+leomartire@users.noreply.github.com>
2020-09-01 14:23:46 +0200

    [SCRIPTS] Update plot script for the fluid-solid validation.

--- EXAMPLES/validation__fluid_solid_coupling__setup/nice_snapshot_mosaic.m
+++ EXAMPLES/validation__fluid_solid_coupling__setup/nice_snapshot_mosaic.m
@@ -6,8 +6,6 @@ setup;
 setup_find_all_cases; % produces istfortho, istfslant, iftsslant, iftsortho
 itodo = [iftsortho, iftsslant, istfortho, istfslant];
 
-
-
 figpath = [plotFolder,filesep,'snapshot_mosaic'];
 
 % t = [460, 754, 1048, 1342]+800;
@@ -50,9 +48,12 @@ end
 
 % Plot.
 fh = figure('units','normalized','outerposition',[0,0,1,1]);
-gaph = 0.02;
-tightAxes = tight_subplot(nlin, ncol, [0.02, gaph], [0.0, 0.0], [0.03, 0.01]);
+gaph = 0.025;
+tightAxes = tight_subplot(nlin, ncol, [0.05, gaph], [0.05, 0.02], [0.03, 0.005]);
 fs=26;
+
+XTCK = [1, 247, 371, 495, size(img{end},2)]; a=(30-0)/(XTCK(4)-XTCK(3)); b = -a*XTCK(3); XTCKLBL = split(sprintf('%.0f|', round(XTCK*a+b)), '|'); XTCKLBL(end)=[];
+YTCK = [13, 296, 337.5, 378, size(img{end},1)-9];a=(10-0)/(YTCK(4)-YTCK(3)); b = -a*YTCK(3); YTCKLBL = split(sprintf('%.0f|', round(YTCK*a+b)), '|'); YTCKLBL(end)=[];
 
 for i = 1:npan
   axes(tightAxes(i));
@@ -60,7 +61,8 @@ for i = 1:npan
   daspect([1,1,1]);
   TIT = ['$t=',sprintf('%.0f', 1e3*t(i)),'$~ms'];
   title(TIT, 'fontsize', fs);
-  xticks({}); yticks({});
+  xticks(XTCK); xticklabels({});
+  yticks(YTCK); yticklabels({});
   if(i==1); ylabel([ftstag, ' orthogonal'], 'fontsize', fs); end
   if(i==5); ylabel([stftag, ' orthogonal'], 'fontsize', fs); end
   if(i==3); ylabel([ftstag, ' slanted'], 'fontsize', fs); end
@@ -70,24 +72,30 @@ end
 hshift = gaph*0.9;
 for i=[2,6]; tightAxes(i).Position = tightAxes(i).Position - [hshift,0,0,0]; end
 for i=[3,7]; tightAxes(i).Position = tightAxes(i).Position + [hshift,0,0,0]; end
+for i=[1,2,5,6]; tightAxes(i).Position = tightAxes(i).Position + [hshift*1.15,0,0,0]; end
+for i=[1:4]; tightAxes(i).Position = tightAxes(i).Position - [0,0.01,0,0]; end
+for i=[5:8]; tightAxes(i).Position = tightAxes(i).Position - [0,0.01,0,0]; end
 
-ll = add_labels_subplots(gcf,0.9,0,[0,-0.07]);
+set(tightAxes(5), 'xticklabel', XTCKLBL);
+set(tightAxes([1,5]), 'yticklabel', YTCKLBL);
+
+ll = add_labels_subplots(gcf,0.9,0,[0,-0.02]);
 
 coeffont = 0.8;
-i = 1; x=300; h = annotateAxes(tightAxes(i), [x, size(img{i},1)-230], [x, size(img{i},1)-450], ['incoming P'], fs*coeffont); set(h, 'color', [1,1,1]);
-i = 5; x=300; h = annotateAxes(tightAxes(i), [x, size(img{i},1)-440], [x, size(img{i},1)-230], ['incoming P'], fs*coeffont); set(h, 'color', [1,1,1]);
+i = 1; x=300; h = annotateAxes(tightAxes(i), [x, size(img{i},1)-200], [x, size(img{i},1)-450], ['incoming P'], fs*coeffont); set(h, 'color', [1,1,1]);
+i = 5; x=300; h = annotateAxes(tightAxes(i), [x, size(img{i},1)-470], [x, size(img{i},1)-230], ['incoming P'], fs*coeffont); set(h, 'color', [1,1,1]);
 
-i = 4; x=320; h = annotateAxes(tightAxes(i), [x, size(img{i},1)-300], [x, size(img{i},1)-150], ['reflected P'], fs*coeffont); set(h, 'color', [1,1,1]);
-i = 4; x=260; h = annotateAxes(tightAxes(i), [x, size(img{i},1)-390], [x, size(img{i},1)-500], ['transmitted S'], fs*coeffont); set(h, 'color', [1,1,1]);
+i = 4; x=320; h = annotateAxes(tightAxes(i), [x, size(img{i},1)-290], [x, size(img{i},1)-150], ['reflected P'], fs*coeffont); set(h, 'color', [1,1,1]);
+i = 4; x=260; h = annotateAxes(tightAxes(i), [x, size(img{i},1)-400], [x, size(img{i},1)-500], ['transmitted S'], fs*coeffont); set(h, 'color', [1,1,1]);
 i = 4; x=400; h = annotateAxes(tightAxes(i), [x, size(img{i},1)-390], [x, size(img{i},1)-450], ['$\quad$','transmitted P'], fs*coeffont); set(h, 'color', [1,1,1]);
 
-i = 7; x=300; h = annotateAxes(tightAxes(i), [x, size(img{i},1)-425], [x, size(img{i},1)-260], ['incoming P'], fs*coeffont); set(h, 'color', [1,1,1]);
-i = 7; x=450; h = annotateAxes(tightAxes(i), [x, size(img{i},1)-485], [x, size(img{i},1)-180], ['incoming S (not studied)'], fs*coeffont); set(h, 'color', [1,1,1]);
+i = 7; x=230; h = annotateAxes(tightAxes(i), [x, size(img{i},1)-425], [x, size(img{i},1)-260], ['incoming P'], fs*coeffont); set(h, 'color', [1,1,1]);
+i = 7; x=450; h = annotateAxes(tightAxes(i), [x, size(img{i},1)-525], [x, size(img{i},1)-180], ['incoming S (not studied)'], fs*coeffont); set(h, 'color', [1,1,1]);
 
-i = 8; x=300; h = annotateAxes(tightAxes(i), [x, size(img{i},1)-285], [x, size(img{i},1)-150], ['transmitted P'], fs*coeffont); set(h, 'color', [1,1,1]);
-i = 8; x=490; h = annotateAxes(tightAxes(i), [x, size(img{i},1)-365], [x, size(img{i},1)-190], ['reflected S'], fs*coeffont); set(h, 'color', [1,1,1]);
-i = 8; x=630; h = annotateAxes(tightAxes(i), [x, size(img{i},1)-375], [x, size(img{i},1)-230], ['reflected P'], fs*coeffont); set(h, 'color', [1,1,1]);
-i = 8; x=310; h = annotateAxes(tightAxes(i), [x, size(img{i},1)-358], [x, size(img{i},1)-500], ['incoming S (not studied)'], fs*coeffont); set(h, 'color', [1,1,1]);
+i = 8; x=260; h = annotateAxes(tightAxes(i), [x, size(img{i},1)-265], [x, size(img{i},1)-150], ['transmitted P'], fs*coeffont); set(h, 'color', [1,1,1]);
+i = 8; x=460; h = annotateAxes(tightAxes(i), [x, size(img{i},1)-375], [x, size(img{i},1)-190], ['reflected S'], fs*coeffont); set(h, 'color', [1,1,1]);
+i = 8; x=610; h = annotateAxes(tightAxes(i), [x, size(img{i},1)-385], [x, size(img{i},1)-230], ['reflected P'], fs*coeffont); set(h, 'color', [1,1,1]);
+i = 8; x=310; h = annotateAxes(tightAxes(i), [x, size(img{i},1)-358], [x, size(img{i},1)-560], ['incoming S (not studied)'], fs*coeffont); set(h, 'color', [1,1,1]);
 
 customSaveFig(fh, figpath, {'fig', 'jpg', 'eps'}, 9999);
 
