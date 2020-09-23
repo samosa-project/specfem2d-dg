a796d140ff867b79ede2a64f5b33c25044568fff
Léo Martire <30411754+leomartire@users.noreply.github.com>
2020-09-23 14:22:45 +0200

    [SCRIPTS] Scripts update (2009231420).

--- utils/utils_specfem2d-dg/load_energy.m
+++ utils/utils_specfem2d-dg/load_energy.m
@@ -1,6 +1,6 @@
 function [t, ke, pe, te] = load_energy(OFDIR)
   energyfilename='energy.dat';
-  energypath = [OFDIR,energyfilename];
+  energypath = [OFDIR,filesep,energyfilename];
   if(not(exist(energypath)==2))
     disp(['[',mfilename,', INFO] Energy file ''',energypath,'''does not exist. Skipping.']);
     return;
